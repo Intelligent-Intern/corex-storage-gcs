15189e9935be0e504e1c435d37c6113fa55ffb13
Jochen Schultz <jschultz@php.net>
2025-03-30 16:26:12 +0200

    Initial commit with GitHub Action workflow

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="corex-storage-gcs",
+    version="0.1.0",
+    author="Jochen Schultz",
+    author_email="js@intelligent-intern.com",
+    packages=["corex_storage_gcs"],
+    install_requires=[],
+    python_requires=">=3.7",
+)
